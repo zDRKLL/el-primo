66bd495ff72cb56f1c5cc24a45f418153c95617b
zDRKLL <152322820+zDRKLL@users.noreply.github.com>
2024-12-02 17:57:33 +0100

    Add files via upload

---
+++ Cricket.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cricket : MonoBehaviour
+{ 
+    // Update is called once per frame
+    void Update()
+    {
+    Debug.Log("cricket");
+    }
+}
